60224d9f2cb0109ad03f0898d4ed70bfa2102f9f
Angus Gibson <angus@agibson.me>
2021-09-20 09:13:39 +1000

    Implement deque constructor, indexing, and Python conversion

--- pythran/pythonic/include/types/deque.hpp
+++ pythran/pythonic/include/types/deque.hpp
@@ -15,8 +15,46 @@ namespace types
     using container_type = std::deque<_type>;
 
     utils::shared_ref<container_type> data;
+
+  public:
+    using reference = typename container_type::reference;
+    using const_reference = typename container_type::const_reference;
+    using iterator = typename container_type::iterator;
+    using size_type = typename container_type::size_type;
+
+    deque();
+    deque(size_type sz);
+    deque(std::initializer_list<T> l);
+
+    template <class InputIterator>
+    deque(InputIterator start, InputIterator stop);
+
+    iterator begin();
+    iterator end();
+
+    reference fast(long n);
+    reference operator[](long n);
+    const_reference fast(long n) const;
+    const_reference operator[](long n) const;
+
+    long size() const;
   };
 }
+
+#ifdef ENABLE_PYTHON_MODULE
+
+template <class T>
+struct to_python<types::deque<T>> {
+  static PyObject *convert(types::deque<T> const &v);
+};
+
+template <class T>
+struct from_python<types::deque<T>> {
+  static bool is_convertible(PyObject *obj);
+  static types::deque<T> convert(PyObject *obj);
+};
+
+#endif
 
 PYTHONIC_NS_END
 
--- pythran/pythonic/types/deque.hpp
+++ pythran/pythonic/types/deque.hpp
@@ -1,12 +1,146 @@
 #ifndef PYTHONIC_TYPES_DEQUE_HPP
 #define PYTHONIC_TYPES_DEQUE_HPP
 
+#include "pythonic/include/types/deque.hpp"
+
+#include "pythonic/utils/shared_ref.hpp"
+
+#include <algorithm>
+
 PYTHONIC_NS_BEGIN
 
 namespace types
 {
-  
+  template <class T>
+  deque<T>::deque()
+    : data(utils::no_memory())
+  {
+  }
+
+  template <class T>
+  deque<T>::deque(deque<T>::size_type sz)
+    : data(sz)
+  {
+  }
+
+  template <class T>
+  deque<T>::deque(std::initializer_list<T> l)
+    : data(std::move(l))
+  {
+  }
+
+  template <class T>
+  template <class InputIterator>
+  deque<T>::deque(InputIterator start, InputIterator stop)
+    : data()
+  {
+    std::copy(start, stop, std::back_inserter(*data));
+  }
+
+  template <class T>
+  typename deque<T>::iterator deque<T>::begin()
+  {
+    return data->begin();
+  }
+
+  template <class T>
+  typename deque<T>::iterator deque<T>::end()
+  {
+    return data->end();
+  }
+
+  template <class T>
+  typename deque<T>::reference deque<T>::fast(long n)
+  {
+    return (*data)[n];
+  }
+
+  template <class T>
+  typename deque<T>::reference deque<T>::operator[](long n)
+  {
+    if (n < 0)
+      n += size();
+    return fast(n);
+  }
+
+  template <class T>
+  typename deque<T>::const_reference deque<T>::fast(long n) const
+  {
+    return (*data)[n];
+  }
+
+  template <class T>
+  typename deque<T>::const_reference deque<T>::operator[](long n) const
+  {
+    if (n < 0)
+      n += size();
+    return fast(n);
+  }
+
+  template <class T>
+  long deque<T>::size() const
+  {
+    return data->size();
+  }
 }
+
+#ifdef ENABLE_PYTHON_MODULE
+
+template <class T>
+PyObject *to_python<types::deque<T>>::convert(types::deque<T> const &v)
+{
+  PyObject *collections_mod = PyImport_ImportModule("collections");
+  PyObject *deque_class = PyObject_GetAttrString(collections_mod, "deque");
+  PyObject *empty_tuple = PyTuple_New(0);
+  PyObject *arg_tuple = PyTuple_New(1);
+
+  PyObject *ret = PyObject_Call(deque_class, empty_tuple, NULL);
+  Py_DECREF(empty_tuple);
+
+  PyObject *append_func = PyObject_GetAttrString(ret, "append");
+
+  for (Py_ssize_t i = 0; i < v.size(); i++) {
+    PyTuple_SetItem(arg_tuple, 0, ::to_python(v[i]));
+    PyObject_Call(append_func, arg_tuple, NULL);
+  }
+
+  Py_DECREF(arg_tuple);
+  Py_DECREF(append_func);
+  Py_DECREF(deque_class);
+  Py_DECREF(collections_mod);
+
+  return ret;
+}
+
+template <class T>
+bool from_python<types::deque<T>>::is_convertible(PyObject *obj)
+{
+  PyObject *collections_mod = PyImport_ImportModule("collections");
+  PyObject *deque_class = PyObject_GetAttrString(collections_mod, "deque");
+
+  bool ret = PyObject_IsInstance(obj, deque_class) &&
+    (PyObject_Not(obj) || ::is_convertible<T>(PySequence_Fast_GET_ITEM(obj, 0)));
+
+  Py_DECREF(deque_class);
+  Py_DECREF(collections_mod);
+
+  return ret;
+}
+
+template <class T>
+types::deque<T> from_python<types::deque<T>>::convert(PyObject *obj)
+{
+  Py_ssize_t l = PySequence_Fast_GET_SIZE(obj);
+  types::deque<T> v(l);
+
+  PyObject **core = PySequence_Fast_ITEMS(obj);
+  std::transform(core, core + l, v.begin(),
+		 [](PyObject *o) { return ::from_python<T>(o); });
+
+  return v;
+}
+
+#endif
 
 PYTHONIC_NS_END
 
