133901531004db0f76ff39754881f1727a8c3d87
Angus Gibson <angus@agibson.me>
2021-09-20 13:26:54 +1000

    fixup! wip! start implementing collections.deque

--- pythran/tests/test_collections.py
+++ pythran/tests/test_collections.py
@@ -5,4 +5,4 @@ class TestDeque(TestEnv):
     def test_deque_(self):
         self.run_test("def deque_(a): from collections import deque; return deque([a])",
                       5,
-                      deque_=[Deque[int]])
+                      deque_=[int])
